180c167f2c1ebc0a45cb9f60236e1d72a0e9c50c
Apoorva Upadhyaya <71746182+apoorva-upadhyaya@users.noreply.github.com>
2022-10-27 23:59:29 +0200

    Add files via upload

---
+++ data/fetchTweetFromID.py
@@ -0,0 +1,44 @@
+import tweepy
+import json
+import os
+import ast
+import pandas as pd
+import csv
+import time
+
+CONSUMER_KEY=""
+CONSUMER_SECRET=""
+OAUTH_TOKEN=""
+OAUTH_TOKEN_SECRET=""
+
+list_ids=[]
+df_stu=pd.read_csv("dataset_2emotion.csv", delimiter=";") 
+print("df_stu :: ",len(df_stu))
+
+list_ids=df_stu["tid"].tolist()
+
+print("len of tweets",len(list_ids),list_ids[:10])
+
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+api = tweepy.API(auth,wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
+
+count=0
+for single_id in list_ids:
+	#print(type(single_id))
+	
+	try:		
+		tweet=api.get_status(single_id, tweet_mode='extended')
+		count=count+1
+		print(count)
+		#<convert <class 'tweepy.models.Status'> to string representation of dict>
+		json_str = json.dumps(tweet._json)
+		#<convert string representation of dict to dict>
+		tweet=json.loads(json_str)
+		with open("tweets_data.json","a") as f:
+			json.dump(tweet,f)
+			f.write("\n")
+	except Exception as e:
+		print("e ::",e)
+		continue
